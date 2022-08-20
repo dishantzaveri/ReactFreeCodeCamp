95cb52296317dc52cca33662bee7c1e6807d4155
dishantzaveri <zaveridishant@gmail.com>
2022-08-21 03:44:27 +0530

    basics

--- index.js
+++ index.js
@@ -3,16 +3,30 @@
 // h1.className = "header"
 
 
-const navbar = (
-    <nav>
-        <h1>dishu</h1>
+// const navbar = (
+//     <nav>
+//         <h1>dishu</h1>
+//         <ul>
+//             <li>1</li>
+//             <li>2</li>
+//             <li>3</li>
+//         </ul>
+
+//     </nav>
+// )
+
+const page = (
+    <div>
+        <h1 className="header">Hello World</h1>
+        <img src='./react-logo.png' width='40px' />
+        <p>React is a JavaScript library for building user interfaces.</p>
         <ul>
             <li>1</li>
             <li>2</li>
             <li>3</li>
         </ul>
+    </div>
 
-    </nav>
 )
 
-ReactDOM.render(navbar, document.getElementById('root'));
+ReactDOM.render(page, document.getElementById('root'));
